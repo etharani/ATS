3fdca0a3a38fc192d2fea9a23cf30c8d130abbce
etharani <tharani2297@gmail.com>
2025-12-12 11:44:18 +0530

    Update to footer
    Update to footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,6 +6,7 @@ import {
   FiInstagram,
   FiTwitter,
   FiLinkedin,
+  FiSearch,
 } from "react-icons/fi";
 
 export default function Footer() {
@@ -55,19 +56,23 @@ export default function Footer() {
 
           <div className="social-icons">
 
-            <a href="#" className="footer-social" title="Facebook">
-              <FiFacebook size={20} />
+            <a
+              href="https://nanthiventures.com/"
+              className="footer-social"
+              title="Website"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FiSearch size={20} />
             </a>
 
-            <a href="#" className="footer-social" title="Instagram">
-              <FiInstagram size={20} />
-            </a>
-
-            <a href="#" className="footer-social" title="Twitter / X">
-              <FiTwitter size={20} />
-            </a>
-
-            <a href="#" className="footer-social" title="LinkedIn">
+            <a
+              href="https://www.linkedin.com/company/nanthiventures/"
+              className="footer-social"
+              title="LinkedIn"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FiLinkedin size={20} />
             </a>
 
